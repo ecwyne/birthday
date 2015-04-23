e9d6095eef5cf5f47ac34765dcfe2d88811e5e9c
ecwyne <ecwyne@gmail.com>
2015-04-23 13:37:15 -0700

    get email working

--- server/method.js
+++ server/method.js
@@ -1,9 +1,19 @@
+if (Meteor.settings.gmail){
+	process.env.MAIL_URL = Meteor.settings.gmail
+}
+
 Meteor.methods({
 	addEmail: function (email){
 		check(email, String);
 		if (!Entries.findOne({email: email})){
 			Entries.insert({email: email});
+			Email.send({
+				to: email,
+				from: 'ecwyne@gmail.com',
+				subject: 'Kathleen\'s Birthday Notes',
+				html: Assets.getText('email.html').replace('EMAIL_ADDRESS', email)
+			});
 		}
-		return email
+		return email;
 	}
 });
