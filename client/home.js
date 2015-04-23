2b2db9e2518fb15d93b6c84fdc8bf67d764c492c
ecwyne <ecwyne@gmail.com>
2015-04-23 13:02:13 -0700

    return key to submit email

--- client/home.js
+++ client/home.js
@@ -1,6 +1,11 @@
 Template.home.events({
-	'keyup #emailAddress': function (){
+	'keypress #emailAddress': function (e){
 		$('.editEntryBtn').attr('disabled', is.not.email($('#emailAddress').val()));
+		if (e.keyCode == 13 && is.email($('#emailAddress').val())){
+			Meteor.call('addEmail', $('#emailAddress').val().toLowerCase(), function (err, data){
+				Router.go('editEntry', {email: data});
+			});
+		}
 	},
 	'click .editEntryBtn': function (){
 		Meteor.call('addEmail', $('#emailAddress').val().toLowerCase(), function (err, data){
